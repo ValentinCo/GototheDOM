02b749747587473307634837da789104b889a096
valentinCo <eersel-val@hotmail.com>
2017-07-06 11:24:23 +0200

    init commit

--- js/app.js
+++ js/app.js
@@ -1,20 +1,4 @@
-console.log('hello world'); 
-  
-    addText.textContent = "Hello world";  //la division addtext contien "hello world"
-    
-
- 	var suprimId = document.getElementById("remove"); 
- 	suprimId.remove();
- 	console.log(suprimId);
- 
-
- 	var changeColor = document.getElementById('changeColor');
- 	console.log(changeColor);
- 	changeColor.style.backgroundColor = "red";
-
- 	var balise = document.getElementById("addChildren");
- 	balise.innerHTML = "<p></p><p><p><p>";
- 	console.log(balise);
+Bonjour
 
  	
 
